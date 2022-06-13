8978694a2a6ad30eb1e42b5c9d0d3bdb3c5120c2
marigold233 <62014410+marigold233@users.noreply.github.com>
2022-06-13 10:04:19 +0800

    Update tsdm_checkin.py

--- tsdm_checkin.py
+++ tsdm_checkin.py
@@ -1,71 +1,25 @@
 # !/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-import sys
-import time
+import logging
+import requests
+import toml
+from parsel import Selector
 from urllib.parse import urljoin
 
-import requests
-import schedule
-import toml
-from loguru import logger
-from parsel import Selector
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 def read_config(config_file):
     config = toml.load(config_file)
-    os = config.get("os")
-    cookies = config.get("cookies")
-    url = config.get("url").get("base_url")
-    task_loop_minutes = config.get("task").get("minutes")
-    message = config.get("message")
-    dingding_url, secret = (
-        message.get("dingding").values()
-        if message.get("dingding").values()
-        else [None, None]
-    )
-    gotify_url = message.get("gotify").get("url")
+    cookies = config.get("COOKIES")
+    url = config.get("URL")
     if not cookies or not url:
         logger.error("未在配置文件找到用户cookie或者天使动漫网址")
         return
-    if dingding_url and secret and gotify_url:
-        return {
-            "os": os,
-            "push_mode": "dingding_and_gotify",
-            "cookies": cookies,
-            "task_loop_minutes": task_loop_minutes,
-            "checkin_url": url,
-            "dingding_url": dingding_url,
-            "dingding_secret": secret,
-            "gotify_url": gotify_url,
-        }
-    elif dingding_url and secret:
-        return {
-            "os": os,
-            "push_mode": "dingding",
-            "cookies": cookies,
-            "task_loop_minutes": task_loop_minutes,
-            "checkin_url": url,
-            "dingding_url": dingding_url,
-            "dingding_secret": secret,
-        }
-    elif gotify_url:
-        return {
-            "os": os,
-            "push_mode": "gotify",
-            "cookies": cookies,
-            "task_loop_minutes": task_loop_minutes,
-            "checkin_url": url,
-            "gotify_url": gotify_url,
-        }
-
-    else:
-        return {
-            "os": os,
-            "push_mode": None,
-            "cookies": cookies,
-            "task_loop_minutes": task_loop_minutes,
-            "checkin_url": url,
-        }
+    return (cookies, url.get("base_url"),)
 
 
 def tsdm_login(cookie):
@@ -76,11 +30,7 @@ def tsdm_login(cookie):
     login_url = urljoin(base_url, "forum.php")
     session = requests.Session()
     session.headers = headers
-    try:
-        login_response = session.get(login_url)
-    except Exception as e:
-        logger.error(e)
-        return
+    login_response = session.get(login_url)
     selector = Selector(text=login_response.text)
     if selector.css("#ls_username") and selector.css("#ls_password"):
         logger.error("cookie已经失效，请重新登录获取cookie")
@@ -93,19 +43,18 @@ def tsdm_work(session):
     response = session.get(work_url)
     tips = Selector(text=response.text).xpath('//*[@id="messagetext"]/p[1]/text()')
     if tips:
-        # logger.info("打工%s" % "".join(tips.getall()))
-        return {"work_info": "打工%s" % "".join(tips.getall()), "work_status": False}
+        logger.info("打工%s" % "".join(tips.getall()))
+        return
     for n in range(1, 7):
         session.post(work_url, data={"act": "clickad"})
-        # logger.info("正在点击第%s广告" % n)
+        logger.info("正在点击第%s广告" % n)
     response = session.post(work_url, data={"act": "getcre"})
     message = "打工完成，%s" % "".join(
         Selector(text=response.text)
         .css("#messagetext.alert_info p")
         .re("<p>(.*?)<br>(.*?)<script")
     )
-    return {"work_info": message, "work_status": True}
-    # logger.info(message)
+    logger.info(message)
 
 
 def checkin(session):
@@ -115,94 +64,20 @@ def checkin(session):
         Selector(text=response.text).xpath('//*[@id="qiandao"]/input/@value').get()
     )
     if not from_hash:
-        return
+        return True
     checkin_api = urljoin(
         base_url, "plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1"
     )
     data = {"formhash": from_hash, "qdxq": "ch", "qdmode": 3, "fastreply": 0}
     checkin_response = session.post(checkin_api, data=data)
-    return True
-
-
-def main(config):
-    global base_url
-    push_mode = config.get("push_mode")
-    cookies = config.get("cookies")
-    base_url = config.get("checkin_url")
-    os = config.get("os")
-
-    def users_checkin(cookies):
-        for user, cookie in cookies.items():
-            session = tsdm_login(cookie)
-            if session:
-                checkin_result = checkin(session)
-                work_result = tsdm_work(session)
-                if checkin_result:
-                    logger.info("TSDM 签到成功！")
-                if work_result.get("work_status") == True:
-                    logger.info(work_result.get("work_info"))
-
-    if push_mode == "dingding" and os == "Linux":
-        logger.info("当前推送消息模式为钉钉")
-        dingding_url = config.get("dingding_url")
-        dingding_secret = config.get("dingding_secret")
-
-        @ding_bot(dingding_url, dingding_secret)
-        def dingding_mode():
-            users_checkin(cookies)
-
-        dingding_mode()
-    elif push_mode == "gotify" and os == "Linux":
-        logger.info("当前推送消息模式为gotify")
-        gotify_url = config.get("gotify_url")
-
-        @gotify(gotify_url, 1)
-        def gotify_mode():
-            users_checkin(cookies)
-
-        gotify_mode()
-    elif push_mode == "dingding_and_gotify" and os == "Linux":
-        logger.info("当前推送消息模式为钉钉和gotify")
-        dingding_url = config.get("dingding_url")
-        dingding_secret = config.get("dingding_secret")
-        gotify_url = config.get("gotify_url")
-
-        @ding_bot(dingding_url, dingding_secret)
-        @gotify(gotify_url, 1)
-        def dingding_and_gotify_mode():
-            users_checkin(cookies)
-
-        dingding_and_gotify_mode()
-    else:
-        logger.info("没有签到打工消息推送")
-        users_checkin(cookies)
+    logger.info("TSDM签到成功")
 
 
 if __name__ == "__main__":
-    log_config = {
-        "handlers": [
-            {"sink": sys.stdout, "format": "{time} - {level} - {message}"},
-            {"sink": "checkin.log"},
-        ],
-        "extra": {"user": "someone"},
-    }
-    logger.configure(**log_config)
-    config_file = "config.toml"
-    config = read_config(config_file)
-    os = config.get("os")
-    task_loop_minutes = config.get("task_loop_minutes")
-    logger.info(f"当前系统平台为{os}, 每{task_loop_minutes}分钟签到和打工一次")
-    if os == "Linux":
-        from message_push import *
-
-        main(config)
-        schedule.every(task_loop_minutes).minutes.do(main, config)
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-    else:
-        main(config)
-        schedule.every(task_loop_minutes).minutes.do(main, config)
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+    global base_url
+    cookies, base_url = read_config("config.toml")
+    for user, cookie in cookies.items():
+        session = tsdm_login(cookie)
+        if session:
+            checkin(session)
+            tsdm_work(session)
